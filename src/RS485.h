255f0108322acfc8e2608ad9a15e0bceaa2bb3aa
mostafa hk <mostafa.hk@gmail.com>
2021-08-30 12:35:38 +0430

    ESP8266 ESP32 compatible

--- src/RS485.h
+++ src/RS485.h
@@ -31,9 +31,20 @@
 #ifdef __AVR__
 #define RS485_DEFAULT_DE_PIN 2
 #define RS485_DEFAULT_RE_PIN -1
+#elif defined(ESP32) || defined(ESP8266)
+#define RS485_DEFAULT_DE_PIN 0
+#define RS485_DEFAULT_RE_PIN 0
 #else
 #define RS485_DEFAULT_DE_PIN A6
 #define RS485_DEFAULT_RE_PIN A5
+#endif
+
+#if defined(ESP8266)
+#define RS485_SER_CONF_TYPE SerialConfig
+#elif defined(ESP32)
+#define RS485_SER_CONF_TYPE uint32_t
+#else
+#define RS485_SER_CONF_TYPE uint16_t
 #endif
 
 
@@ -45,9 +56,9 @@ class RS485Class : public Stream {
     RS485Class(HardwareSerial& hwSerial, int txPin, int dePin, int rePin);
 
     virtual void begin(unsigned long baudrate);
-    virtual void begin(unsigned long baudrate, uint16_t config);
+    virtual void begin(unsigned long baudrate, RS485_SER_CONF_TYPE config);
     virtual void begin(unsigned long baudrate, int predelay, int postdelay);
-    virtual void begin(unsigned long baudrate, uint16_t config, int predelay, int postdelay);
+    virtual void begin(unsigned long baudrate, RS485_SER_CONF_TYPE config, int predelay, int postdelay);
     virtual void end();
     virtual int available();
     virtual int peek();
