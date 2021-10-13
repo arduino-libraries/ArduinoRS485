e056966d7cd2fc375e530967b8f11a9f8a20d56a
mostafa hk <mostafa.hk@gmail.com>
2021-10-13 17:52:00 +0330

    esp32/8266 compatible fix

--- src/RS485.h
+++ src/RS485.h
@@ -21,35 +21,8 @@
 #define _RS485_H_INCLUDED
 
 #include <Arduino.h>
+#include "RS485_defs.h"
 
-#ifdef PIN_SERIAL1_TX
-#define RS485_DEFAULT_TX_PIN PIN_SERIAL1_TX
-#else
-#define RS485_DEFAULT_TX_PIN 1 
-#endif
-
-#ifdef __AVR__
-#define RS485_DEFAULT_DE_PIN 2
-#define RS485_DEFAULT_RE_PIN -1
-#elif defined(ESP32) || defined(ESP8266)
-#define RS485_DEFAULT_DE_PIN 0
-#define RS485_DEFAULT_RE_PIN 0
-#else
-#define RS485_DEFAULT_DE_PIN A6
-#define RS485_DEFAULT_RE_PIN A5
-#endif
-
-#if defined(ESP8266)
-#define RS485_SER_CONF_TYPE SerialConfig
-#elif defined(ESP32)
-#define RS485_SER_CONF_TYPE uint32_t
-#else
-#define RS485_SER_CONF_TYPE uint16_t
-#endif
-
-
-#define RS485_DEFAULT_PRE_DELAY 50
-#define RS485_DEFAULT_POST_DELAY 50
 
 class RS485Class : public Stream {
   public:
